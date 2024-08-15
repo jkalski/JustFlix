b2663c751e124aa7b50b7de8fb1edd74ea21829d
justinkalski <jkalski.cty@gmail.com>
2024-08-14 22:09:36 -0700

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+import authRoutes from './routes/auth.route.js';
+import { ENV_VARS } from './config/envVars.js';
+import { connectDB } from './config/db.js';
+
+
+const app = express();
+
+const PORT = ENV_VARS.PORT;
+
+//console.log('Mongo URI:', ENV_VARS.MONGO_URI); // Check if the URI is logged correctly
+
+app.use("/api/v1/auth", authRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
+    connectDB();
+});
