352ed1b7758e6cf7d3625a2adb92f4db6beb55b6
justinkalski <jkalski.cty@gmail.com>
2024-08-15 17:14:51 -0700

    Add server setup, movie routes, and TMDB service
    - Implemented  for server configuration.
    - Added movie routes and controllers.
    - Integrated TMDB API in .

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,25 @@
 import express from 'express';
 
 import authRoutes from './routes/auth.route.js';
+import movieRoutes from './routes/movie.route.js';
+
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 
 
 const app = express();
 
-const PORT = ENV_VARS.PORT;
-
-//console.log('Mongo URI:', ENV_VARS.MONGO_URI); // Check if the URI is logged correctly
+const PORT = ENV_VARS.PORT; 
 
 app.use(express.json()); // will allow us to parse req. body in JSON format
+
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
 
 
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
     connectDB();
 });
+
+
