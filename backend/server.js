2fb67c0004ec9b529f91cb091178f494af7c6315
justinkalski <jkalski.cty@gmail.com>
2024-08-15 22:16:02 -0700

    Add authentication endpoints: signup, login, and logout
    - Includes user signup with validation and hashing.
    - Adds login with JWT and logout functionality.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js';
 import movieRoutes from './routes/movie.route.js';
 import tvRoutes from './routes/tv.route.js';
+import searchRoutes from './routes/search.route.js';
 
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
+import { protectRoute } from './middleware/protectRoute.js';
+
 
 
 const app = express();
@@ -13,10 +17,12 @@ const app = express();
 const PORT = ENV_VARS.PORT; 
 
 app.use(express.json()); // will allow us to parse req. body in JSON format
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
 
 app.listen(PORT, () => {
