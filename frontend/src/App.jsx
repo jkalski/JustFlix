c7f610828193303f9b61cd126a24b87d88257ebc
justinkalski <jkalski.cty@gmail.com>
2024-08-19 16:32:50 -0700

    Update SearchPage to route all results to /watch/:id for unified handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
+import SearchPage from "./pages/SearchPage";
 import WatchPage from "./pages/WatchPage";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
@@ -35,6 +36,8 @@ console.log("auth user is here: ", user);
         <Route path='/login' element={!user ?<LoginPage /> : <Navigate to={"/"}/> } />
         <Route path='/signup' element={!user ? <SignUpPage /> : <Navigate to ={"/"} />} />
         <Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to ={"/login"} />} />
+        <Route path='/search' element={user ? <SearchPage /> : <Navigate to ={"/login"} />} />
+
       </Routes>
       <Footer />
       
